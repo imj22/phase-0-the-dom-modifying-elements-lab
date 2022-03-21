1a3299f7e6e62773fb45eb054577e62e22847047
imj22 <98693440+imj22@users.noreply.github.com>
2022-03-21 14:09:23 +0100

    commit tree 1bb5902f6fd7d368f5df8ca944553b1429f71cdc

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+document.querySelector("main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// newHeader.append = "YOUR-NAME is the champion"
+
